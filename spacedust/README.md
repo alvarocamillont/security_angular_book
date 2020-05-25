2ca2c526c6fccbec9e30fe880553202ce64a57bb
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2020-05-25 16:17:31 -0300

    Inclusão do projeto

---
+++ spacedust/README.md
@@ -0,0 +1,23 @@
+# Spacedust Front End
+
+## Getting Started
+
+Welcome to SpaceDust, the sample front end app for Securing Angular Applications!
+
+Start by installing the dependencies. Make sure you have Node installed and then run:
+
+```bash
+npm install
+```
+
+## Run the App
+
+Run the app with the Angular CLI
+
+```bash
+ng serve
+```
+
+## Get an mLab Account
+
+The login and signup transactions won't work until you have set up the API and connected to mLab. See the readme for the Node.js API for instructions on that.
---
+++ spacedust/src/app/app.routes.ts
@@ -0,0 +1,24 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { ExoplanetComponent } from './exoplanet/exoplanet.component';
+import { AuthGuardService as AuthGuard } from './auth/auth-guard.service';
+import { RoleGuardService as RoleGuard } from './auth/role-guard.service';
+import { ExoplanetNewComponent } from './exoplanet-new/exoplanet-new.component';
+
+export const ROUTES: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'catalog', component: CatalogComponent, canActivate: [AuthGuard] },
+  {
+    path: 'exoplanet/:id',
+    component: ExoplanetComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'new-exoplanet',
+    component: ExoplanetNewComponent,
+    canActivate: [RoleGuard],
+    data: { expectedRole: 'admin' }
+  },
+  { path: '**', redirectTo: 'catalog' }
+];
